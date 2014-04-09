ab3924c80b85eeba599df52f01ad8543ed7a3c9c
FakeYou <ag.nanninga@gmail.com>
2014-04-09 15:47:41 +0200

    fixed reversie module

---
+++ reversi/src/main/java/org/kratos/reversi/App.java
@@ -0,0 +1,14 @@
+package org.kratos.reversi;
+
+import org.kratos.framework.Kratos;
+
+/**
+ * Created by FakeYou on 9-4-14.
+ */
+public class App {
+
+    public App(Kratos kratos) {
+
+    }
+
+}
